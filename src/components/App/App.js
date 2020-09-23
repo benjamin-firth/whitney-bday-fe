7529036aa8f3732d4ab73f8a56e88654a351911a
Ben Firth <bfirth93@gmail.com>
2020-09-22 20:16:53 -0600

    Basic jsx for app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h3>How many people are partying?!</h3>
+      <h1>1</h1>
+      <div>
+        <h2>-</h2>
+        <h2>+</h2>
+      </div>
+    </main>
   );
 }
 
